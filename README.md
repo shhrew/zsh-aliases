997cfc229320c684fd5dbb069353dd2635c2818e
shhrew <shhrew@proton.me>
2024-08-25 22:11:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# zsh-aliases
+
+Clone this repo in your home folder and run the below command.
+
+After restarting zsh you should be able to use all aliases and scripts.
+
+```bash
+echo "source ~/zsh-aliases/aliases.zsh" >> ~/.zshrc
+```
---
+++ aliases.zsh
@@ -0,0 +1,55 @@
+# Misc
+ips() {
+  if [ -z "$1" ]; then
+      ip a show scope global | awk '/^[0-9]+:/ { sub(/:/,"",$2); iface=$2 } /^[[:space:]]*inet / { split($2, a, "/"); print "[\033[96m" iface"\033[0m] "a[1] }'
+      return
+  fi
+
+  # Adapter specified by the user
+  ADAPTER=$1
+
+  # Check if the specified adapter exists
+  if ifconfig $ADAPTER > /dev/null 2>&1; then
+      # Extract the IP address and copy it to the clipboard
+      IP_ADDRESS=$(ifconfig $ADAPTER | grep 'inet ' | awk '{print $2}' | tr -d '\n')
+
+      # Check if IP_ADDRESS is not empty
+      if [ -n "$IP_ADDRESS" ]; then
+          echo -n "$IP_ADDRESS" | xclip -sel clip
+          print "[\033[96m"$ADAPTER"\033[0m] $IP_ADDRESS copied to clipboard!"
+      else
+          print "[\033[96m"$ADAPTER"\033[0m] No IP address found for adapter"
+      fi
+  fi
+}
+
+lspwd() {
+  echo -e "[\e[96m`pwd`\e[0m]\e[34m" && ls && echo -en "\e[0m"
+}
+
+mkdircd() {
+  mkdir $1 && cd $_
+}
+
+addhost() {
+    if [ "$#" -ne 2 ]; then
+      echo "[i] Usage: addhost <ip> <hostname>"
+      return 1
+    fi
+
+    ip="$1"
+    hostname="$2"
+    if grep -q "^$ip" /etc/hosts; then
+      sudo sed -i "/^$ip/s/$/ $hostname/" /etc/hosts
+      print "[\033[96m"+"\033[0m] Appended $hostname to existing entry for $ip in /etc/hosts"
+    else
+      echo "$ip $hostname" | sudo tee -a /etc/hosts > /dev/null
+      print "[\033[96m"+"\033[0m] Added new entry: $ip $hostname to /etc/hosts"
+    fi
+
+    grep "^$ip" /etc/hosts
+}
+
+alias www="lspwd && sudo python3 -m http.server 80"
+alias stty_fix="stty raw -echo; fg; reset"
+alias stty_conf="stty -a | sed 's/;//g' | head -n 1 | sed 's/.*baud /stty /g;s/line.*//g' | xclip -sel clip"
