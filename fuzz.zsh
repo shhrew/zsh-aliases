ec7318c93d47d04fd212bc3a394dca38f226475c
shhrew <shhrew@proton.me>
2024-09-09 01:22:50 +0100

    feat: added vhost fuzz

--- fuzz.zsh
+++ fuzz.zsh
@@ -1,1 +1,19 @@
 alias enumports="python -c 'print(\"\n\".join(str(i) for i in range(1, 65536)))'"
+
+vhost() {
+    if [ "$#" -lt 1 ]; then
+        echo "[i] Usage: vhost <domain> (extra arguments)"
+        return 1
+    fi
+
+    local domain=$1
+    if [[ ! "$domain" =~ ^https?:// ]]; then
+        url="http://$domain"
+    else
+        url=$domain
+        domain=${url/https:\/\//} # ${main_string/search_term/replace_term}
+    fi
+
+    local wordlist="$HOME/wordlists/seclists/Discovery/DNS/bitquark-subdomains-top100000.txt"
+    ffuf -H "Host: FUZZ.$domain" -u $url -w "$wordlist" "${@:2}"
+}
